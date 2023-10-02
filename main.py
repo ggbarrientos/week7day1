12fd956f46f092ee266afaea752ab7922a195239
ggbarrientos <143198944+ggbarrientos@users.noreply.github.com>
2023-10-02 13:59:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#working with apis dictionaires
+import requests
+import json
+response = requests.get("https://randomuser.me/api")
+#print(response.json())
+
+title = response.json()['results'][0]['name']['first']
+print(title)
+gender = response.json()['results'][0]['gender']
+print(gender)
+lastname = response.json()['results'][0]['name']['last']
+print(lastname)
+street = response.json()['results'][0]['location']['street']['name']
+print(street)
+city= response.json()['results'][0]['location']['city']
+print(city)
+state = response.json()['results'][0]['location']['state']
+print(state)
+
+country = response.json()['results'][0]['location']['country']
+print(country)
+
+postcode = response.json()['results'][0]['location']['postcode']
+print(postcode)
+email = response.json()['results'][0]['email']
+print(email)
+username = response.json()['results'][0]['login']['username']
+print(username)
+birth = response.json()['results'][0]['dob']['date']
+print(birth)
